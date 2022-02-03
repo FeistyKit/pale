3e642191f563cc07f1a15a3fe40d1f2e3d51c57d
FeistyKit <eeveebeevee33@gmail.com>
2022-02-03 07:41:19 -0500

    moving to other computer—started working on tokenizer

--- src/lib/tokens.rs
+++ src/lib/tokens.rs
@@ -10,9 +10,9 @@ pub struct Token {
 
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub struct Location {
-    pub(crate) filename: String,
-    pub(crate) line: usize,
-    pub(crate) col: usize,
+    pub filename: String,
+    pub line: usize,
+    pub col: usize,
 }
 
 impl Display for Location {
@@ -27,6 +27,12 @@ pub(crate) enum TokenType {
     CloseParens,
     Recognizable(LispType),
     Ident(String),
+}
+
+impl TokenType {
+    fn new_str_lit(source: String) -> Self {
+        Self::Ident(source)
+    }
 }
 
 impl<T: ToString> From<T> for TokenType {
@@ -54,14 +60,79 @@ fn guess_capacity(input: &str) -> usize {
     input.len() / 5
 }
 
+#[derive(Debug)]
+enum TokenizerStatus {
+    String,
+    Normal,
+}
+
+#[derive(Debug)]
+struct Tokenizer {
+    to_return: Vec<Token>,
+    pos: (usize, usize),
+    pos_locked: bool,
+    token_buf: String,
+    status: TokenizerStatus,
+    default_buf_len: usize,
+    filename: String,
+}
+
+impl Tokenizer {
+    fn new(filename: String) -> Self {
+        // This number can and might change, or I might change the method of getting it.
+        let default_buf_len = 16;
+        Tokenizer {
+            to_return: Vec::with_capacity(default_buf_len),
+            pos: (0, 0),
+            pos_locked: false,
+            token_buf: String::with_capacity(default_buf_len),
+            status: TokenizerStatus::Normal,
+            default_buf_len,
+            filename,
+        }
+    }
+
+    fn push_tok(&mut self) {
+
+        match self.status {
+            TokenizerStatus::Normal => {
+                if self.token_buf.trim() != "" {
+                    let tok = Token {
+                        loc: Location {
+                            line: self.pos.1,
+                            col: self.pos.0,
+                            filename: self.filename.clone(),
+                        },
+                        dat: self.token_buf.into(),
+                    };
+                    self.to_return.push(tok);
+                    self.token_buf = String::with_capacity(self.default_buf_len);
+                    self.pos_locked = false;
+                }
+            }
+
+            TokenizerStatus::String => {
+                let tok = Token {
+                    loc: Location {
+                        line: self.pos.1,
+                        col: self.pos.0,
+                        filename: self.filename.clone(),
+                    },
+                    dat: TokenType::new_str_lit(self.token_buf.clone()),
+                };
+                self.to_return.push(tok);
+                self.token_buf = String::with_capacity(self.default_buf_len);
+                self.pos_locked = false;
+                self.status = TokenizerStatus::Normal;
+            }
+
+        }
+    }
+
+    fn tokenize(input: &str, )
+}
+
 pub(crate) fn tokenize(input: &str, name: &str) -> Result<Vec<Token>, String> {
-    let mut to_return = Vec::with_capacity(guess_capacity(input));
-
-    let mut token_buf = String::with_capacity(16);
-    let mut token_col = 0;
-    let mut token_line = 0;
-
-    let mut in_string = false;
     for (line_number, line_data) in input.lines().enumerate() {
         for (col_number, character) in line_data.trim().char_indices() {
             match (character, in_string) {
@@ -91,22 +162,7 @@ pub(crate) fn tokenize(input: &str, name: &str) -> Result<Vec<Token>, String> {
                     token_col = col_number;
                     token_line = line_number;
                 }
-                (' ', false) => {
-                    if token_buf.trim() != "" {
-                        let tok = Token {
-                            loc: Location {
-                                line: token_line,
-                                col: token_col,
-                                filename: name.to_string(),
-                            },
-                            dat: token_buf.into(),
-                        };
-                        to_return.push(tok);
-                        token_buf = String::with_capacity(16);
-                        token_col = col_number + 1;
-                        token_line = line_number;
-                    }
-                }
+                (' ', false) => {}
                 ('(', false) => {
                     let tok = Token {
                         loc: Location {
